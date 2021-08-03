ad9c00d61c1b6045e0e7a47e8db9030cfd4e918c
Kai Kuehner <kvk@pobox.com>
2021-08-03 00:11:01 -0400

    WIP bouncing

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,6 +8,8 @@ public class Player : MonoBehaviour
     private const float jumpForce = 10;
     private const float doubleJumpForce = 10;
     private const float gravityForce = 20;
+    private const float slamForce = 40;
+    private const float minBounceForce = 5;
 
     private Rigidbody2D rb;
     private EdgeCollider2D ec;
@@ -16,6 +18,7 @@ public class Player : MonoBehaviour
     private bool slamQueued = false;
     private bool canDoubleJump = true;
     private bool isSlamming = false;
+    private float xForce = 0;
 
     private SpriteRenderer sr;
 
@@ -55,6 +58,45 @@ public class Player : MonoBehaviour
         float xInput = Input.GetAxis("Horizontal");
         float xVel = xInput * speed;
 
+        if (xForce != 0)
+		{
+            //if not moving: keep xForce
+            if (xInput == 0)
+			{
+                xVel = xForce;
+			}
+            else
+			{
+                if (Mathf.Sign(xVel) == Mathf.Sign(xForce)) {
+                    //moving in same direction
+                    if (Mathf.Abs(xVel) >= Mathf.Abs(xForce))
+					{
+                        //xVel has higher magnitude: set xForce to 0 (replace little momentum push)
+                        xForce = 0;
+                    }
+                    else
+					{
+                        //xForce has higher magnitude: set xVel to xForce (pushed by higher momentum)
+                        xVel = xForce;
+                    }
+                }
+                else
+				{
+                    //moving in other direction
+                    //decrease xForce by xVel (stopping at 0)
+                    float prevSign = Mathf.Sign(xForce);
+                    xForce += xVel / 4;
+                    if (Mathf.Sign(xForce) != prevSign)
+					{
+                        xForce = 0;
+					}
+                    xVel = xForce;
+                }
+            }
+		}
+
+        float yVel;
+
         bool onGround = CheckSide(4, 3, Vector2.down);
         bool onCeiling = CheckSide(1, 2, Vector2.up);
 
@@ -66,12 +108,20 @@ public class Player : MonoBehaviour
             }*/
             canDoubleJump = true;
 
+            isSlamming = false;
+            xForce = 0;
+            yVel = 0;
+
             if (rb.velocity.y < 0)
             {
                 //PlaySound(bonkSound);
             }
         }
-        float yVel = onGround ? 0 : rb.velocity.y - gravityForce * Time.fixedDeltaTime;
+        else
+		{
+            yVel = rb.velocity.y - gravityForce * Time.fixedDeltaTime;
+        }
+
         if (onCeiling && yVel > 0)
         {
             yVel = 0;
@@ -83,12 +133,12 @@ public class Player : MonoBehaviour
             jumpQueued = false;
             if (onGround)
             {
-                yVel = jumpForce;
+                yVel = Mathf.Max(jumpForce, yVel + jumpForce);
                 //PlaySound(jumpSound);
             }
             else if (canDoubleJump)
 			{
-                yVel = doubleJumpForce;
+                yVel = Mathf.Max(doubleJumpForce, yVel + doubleJumpForce);
                 //PlaySound(doubleJumpSound);
                 canDoubleJump = false;
 			}
@@ -98,6 +148,12 @@ public class Player : MonoBehaviour
 		{
             slamQueued = false;
             print("slam");
+            isSlamming = true;
+		}
+
+        if (isSlamming)
+		{
+            yVel = -slamForce;
 		}
 
         Vector2 vel = new Vector2(xVel, yVel);
@@ -125,5 +181,47 @@ public class Player : MonoBehaviour
                 sr.sprite = standImage;
             }
         }*/
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject collider = collision.collider.gameObject;
+
+        if (collider.layer == LayerMask.NameToLayer("Tiles"))
+		{
+            if (collision.GetContact(0).normal.x != 0)
+            {
+                //against wall, not ceiling
+                xForce = 0;
+            }
+        }
+
+        Bouncer bouncer = collider.GetComponent<Bouncer>();
+        if (bouncer != null)
+        {
+            //PlaySound(bounceSound);
+            isSlamming = false;
+            canDoubleJump = true;
+            //TODO add scaled bounce x force
+            //TODO clamp to min/max y for bouncing vertical? (terminal velocity)
+            Vector2 playerPos = rb.position + new Vector2(0, ec.points[0].y);
+            Vector2 bouncerPos = new Vector2(collider.transform.position.x, collider.transform.position.y);
+            Vector2 bouncerToPlayer = (playerPos - bouncerPos).normalized;
+            print(bouncerToPlayer);
+            float bounceYVel = -rb.velocity.y * bouncer.bounceForce * Mathf.Abs(bouncerToPlayer.y);
+            if (bouncerToPlayer.y >= 0 && bounceYVel < minBounceForce)
+			{
+                bounceYVel = minBounceForce;
+            }
+            if (bouncerToPlayer.y < 0 && bounceYVel > -minBounceForce)
+            {
+                bounceYVel = -minBounceForce;
+            }
+            float bounceXVel = Mathf.Abs(rb.velocity.y) * bouncer.bounceForce * bouncerToPlayer.x;
+            xForce = bounceXVel;
+            print("bounce " + bounceXVel + ", " + bounceYVel);
+            //rb.velocity = new Vector2(rb.velocity.x, bounceYVel);
+            rb.velocity = new Vector2(bounceXVel, bounceYVel);
+        }
     }
 }
--- README.md
+++ README.md
@@ -5,19 +5,24 @@ For AGBIC 2021: https://itch.io/jam/a-game-by-its-cover-2021
 Concept by Alice Pedersen (@bakumoe): https://famicase.com/21/softs/010.html
 
 ## TODO
-
-- double jump (flap)
-	- crystals to refill double jump in midair
-- downslam
-	- break sugar floors
-	- get bounced off of donuts
-	- combat?
-- collect sprinkles
-- keep momentum- speedrun routes
-
-- other angels: frosting (dark skin pink hair), cinnamon
-
+- Movement
+	- Coyote time & jump queueing
+	- X acceleration
+- Mechanics
+	- double jump (flap)
+		- crystals to refill double jump in midair
+	- downslam
+		- break sugar floors
+		- get bounced off of donuts
+		- combat?
+	- collect sprinkles
+- Juice
+	- Short pause before slam
+	- Bouncers jiggle when bounced on
 - levels:
 	- cake cliffs
 	- jelly swamp
-	- donut heaven
+	- donut heaven (boss?)
+	- keep momentum- speedrun routes
+- ending
+	- other angels: frosting (dark skin pink hair), cinnamon
